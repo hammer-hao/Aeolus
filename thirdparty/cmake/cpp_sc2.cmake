15504a96dbf67940cfb406144651d751407303de
hammer-hao <zihehaomichael@gmail.com>
2024-12-09 23:32:18 -0500

    added the renderer

--- thirdparty/cmake/cpp_sc2.cmake
+++ thirdparty/cmake/cpp_sc2.cmake
@@ -6,7 +6,7 @@ set(BUILD_API_EXAMPLES OFF CACHE INTERNAL "" FORCE)
 # Dont build tests in the sc2api submodule.
 set(BUILD_API_TESTS OFF CACHE INTERNAL "" FORCE)
 
-# Dont build sc2renderer in the sc2api submodule.
+# dont build sc2renderer in the sc2api submodule.
 set(BUILD_SC2_RENDERER OFF CACHE INTERNAL "" FORCE)
 
 include(FetchContent)
@@ -18,7 +18,28 @@ FetchContent_Declare(
 )
 FetchContent_MakeAvailable(cpp_sc2)
 
+# Get the source directory of cpp-sc2
+set(CPP_SC2_DIR ${cpp-sc2_SOURCE_DIR})
+
+# Verify the directory
+message(STATUS "CPP_SC2_DIR: ${CPP_SC2_DIR}")
+
+set(SC2_RENDERER_DIR "${CPP_SC2_DIR}/src/sc2renderer")
+message(STATUS "SC2_RENDERER_DIR: ${SC2_RENDERER_DIR}")
+
+file(GLOB SC2_RENDERER_SOURCES
+    "${SC2_RENDERER_DIR}/*.cc"
+    "${SC2_RENDERER_DIR}/*.h"
+)
+
+message(STATUS "SC2_RENDERER_SOURCES: ${SC2_RENDERER_SOURCES}")
+
 set_target_properties(sc2api PROPERTIES FOLDER cpp-sc2)
 set_target_properties(sc2lib PROPERTIES FOLDER cpp-sc2)
 set_target_properties(sc2protocol PROPERTIES FOLDER cpp-sc2)
 set_target_properties(sc2utils PROPERTIES FOLDER cpp-sc2)
+
+# Only set properties for sc2renderer if it is being built
+if (BUILD_SC2_RENDERER)
+    set_target_properties(sc2renderer PROPERTIES FOLDER cpp-sc2)
+endif()
--- thirdparty/cmake/sdl2.cmake
+++ thirdparty/cmake/sdl2.cmake
@@ -11,6 +11,10 @@ FetchContent_Declare(
 
 FetchContent_MakeAvailable(sdl2)
 
+set(SDL2_DISABLE_INSTALL ON CACHE BOOL "" FORCE)
+set(SDL2_BUILD_STATIC ON CACHE BOOL "" FORCE)
+set(SDL2_BUILD_SHARED OFF CACHE BOOL "" FORCE)
+
 # Set global variables
 set(SDL2_INCLUDE_DIR ${sdl2_SOURCE_DIR}/include CACHE INTERNAL "SDL2 include directory")
 set(SDL2_LIBRARIES SDL2::SDL2 SDL2::SDL2main CACHE INTERNAL "SDL2 libraries")
