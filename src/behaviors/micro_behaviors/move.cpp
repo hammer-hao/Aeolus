40ec7d39a53ccb1e0000c5a36534ba6f0bb05af1
hammer-hao <zihehaomichael@gmail.com>
2025-02-05 16:32:56 -0500

    fix: now uses `GENERAL_MOVE` to replace legacy `MOVE_MOVE` for move commands

--- src/behaviors/micro_behaviors/move.cpp
+++ src/behaviors/micro_behaviors/move.cpp
@@ -14,14 +14,17 @@ namespace Aeolus
 		// std::cout << "Existing order size: " << unit->orders.size() << std::endl;
 		if (unit->orders.size() == 1)
 		{
-			// std::cout << "Existing order: " << ::sc2::AbilityTypeToName(unit->orders.front().ability_id) << std::endl;
-			if (unit->orders.front().ability_id == ::sc2::ABILITY_ID::MOVE_MOVE
+			//std::cout << "Existing order: " << ::sc2::AbilityTypeToName(unit->orders.front().ability_id) << std::endl;
+			// std::cout << "Existing order target: " << unit->orders.front().target_pos.x << " " << unit->orders.front().target_pos.y
+			//	<< std::endl;
+			// std::cout << "New order target: " << m_target.x << " " << m_target.y << std::endl;
+ 			if (unit->orders.front().ability_id == ::sc2::ABILITY_ID::GENERAL_MOVE
 				&& unit->orders.front().target_pos == m_target)
 				// already performing the exact command
 				// std::cout << "duplicate command detected!" << std::endl;
 				return true;
 		}
-		aeolusbot.Actions()->UnitCommand(unit, ::sc2::ABILITY_ID::MOVE_MOVE, m_target);
+		aeolusbot.Actions()->UnitCommand(unit, ::sc2::ABILITY_ID::GENERAL_MOVE, m_target);
 		return true;
 	}
 }
