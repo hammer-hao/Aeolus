bde688dc75a37710fa548e0b30c2cfb029850ee0
hammer-hao <zihehaomichael@gmail.com>
2025-04-17 19:29:29 -0400

    feat: now will contruct a pylon first if structure in build order has no valid placement

--- src/buildorder/structurebuildorderstep.cpp
+++ src/buildorder/structurebuildorderstep.cpp
@@ -24,13 +24,46 @@ namespace Aeolus
 		if (aeolusbot.Observation()->GetFoodUsed() >= m_supply_threshold)
 		{
 			m_num_before = 0;
-			for (const auto& structure : ManagerMediator::getInstance().GetAllOwnStructures(aeolusbot))
+
+			::sc2::Units allStructures = ManagerMediator::getInstance().GetAllOwnStructures(aeolusbot);
+			for (const auto& structure : allStructures)
 			{
 				if (structure->unit_type == m_to_build) m_num_before++;
 			}
-			std::make_unique<BuildStructure>(m_to_build, m_base_location, m_is_wall).get()->execute(aeolusbot);
-			m_started = true;
-			return true;
+			if (!std::make_unique<BuildStructure>(m_to_build, m_base_location, m_is_wall).get()->execute(aeolusbot))
+			{
+				// if what we are trying to build was not a nexus / assimilator / pylon, then it means
+				// no available position at the base we are in
+				// in that case, let's build a pylon first
+				if (m_to_build != ::sc2::UNIT_TYPEID::PROTOSS_NEXUS &&
+					m_to_build != ::sc2::UNIT_TYPEID::PROTOSS_ASSIMILATOR &&
+					m_to_build != ::sc2::UNIT_TYPEID::PROTOSS_PYLON)
+				{
+					int numPendingPylons = ManagerMediator::getInstance().GetNumberPending(aeolusbot, ::sc2::UNIT_TYPEID::PROTOSS_PYLON);
+					if (numPendingPylons == 0)
+					{
+						for (const auto& structure : allStructures)
+						{
+							if (structure->unit_type == ::sc2::UNIT_TYPEID::PROTOSS_PYLON && structure->build_progress < 1.0f)
+							{
+								numPendingPylons++;
+								break;
+							}
+						}
+						if (numPendingPylons == 0)
+						{
+							// no pylons queued / in progress, build one
+							std::make_unique<BuildStructure>(::sc2::UNIT_TYPEID::PROTOSS_PYLON, m_base_location, m_is_wall).get()->execute(aeolusbot);
+						}
+					}
+				}
+				return false;
+			}
+			else
+			{
+				m_started = true;
+				return true;
+			}
 		}
 		return false;
 	}
