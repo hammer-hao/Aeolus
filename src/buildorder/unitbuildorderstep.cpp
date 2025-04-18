ee8942e3160ec4b7d6dedceec215b98efaf23fde
hammer-hao <zihehaomichael@gmail.com>
2025-04-17 23:24:03 -0400

    Update unitbuildorderstep.cpp

--- src/buildorder/unitbuildorderstep.cpp
+++ src/buildorder/unitbuildorderstep.cpp
@@ -33,23 +33,37 @@ namespace Aeolus
 				return unit->unit_type == trainedFrom && unit->build_progress >= 1.0 && unit->orders.empty(); 
 			});
 
-		if (canTrain.empty()) return false; // no building available to train
+		if (canTrain.empty())
+		{
+			std::cout << "no building available to train!" << std::endl;
+			return false; // no building available to train
+		}
 
 		// need to make sure we have required tech
 		::sc2::UNIT_TYPEID required_tech = mediator.GetRequiredTech(aeolusbot, m_to_train);
 		if (!std::any_of(all_structures.begin(), all_structures.end(),
 			[required_tech](const ::sc2::Unit* structure) {return structure->unit_type == required_tech; }))
+		{
+			std::cout << "we do not have the required tech to train " << ::sc2::UnitTypeToName(m_to_train) << std::endl;
+			std::cout << "required tech: " << ::sc2::UnitTypeToName(required_tech) << std::endl;
 			return false; // does not have required tech yet.
+		}
 
 		auto unitCost = mediator.GetUnitCost(aeolusbot, m_to_train);
 		if (mediator.GetMinerals(aeolusbot) < unitCost.first
 			|| mediator.GetVespene(aeolusbot) < unitCost.second)
+		{
+			std::cout << "Not enough resources! current minerals: " << mediator.GetMinerals(aeolusbot)
+				<< " current gas: " << mediator.GetVespene(aeolusbot) << std::endl;
 			return false; // not enough resources to afford this unit.
+		}
 
 		for (const auto& structure : canTrain)
 		{
 			{
 				// found available structure
+				std::cout << "Issueing training command to " << ::sc2::UnitTypeToName(structure->unit_type)
+					<< " to train " << ::sc2::UnitTypeToName(m_to_train) << std::endl;
 				aeolusbot.Actions()->UnitCommand(structure,
 					ManagerMediator::getInstance().GetCreationAbility(aeolusbot, m_to_train));
 				m_started = true;
