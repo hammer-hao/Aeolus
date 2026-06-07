9f76a890e7dd992611d8ed986b39cef79130173b
hammer-hao <zihehaomichael@gmail.com>
2026-06-06 23:28:49 -0400

    feat: auto supply forces a pylon if none on the map

--- src/behaviors/macro_behaviors/auto_supply.cpp
+++ src/behaviors/macro_behaviors/auto_supply.cpp
@@ -17,45 +17,74 @@ namespace Aeolus
 		return false;
 	}
 
-	int AutoSupply::_numSupplyRequired(AeolusBot& aeolusbot)
-	{
-		auto* observation = aeolusbot.Observation();
-		auto& mediator = ManagerMediator::getInstance();
-		int current_cap = observation->GetFoodCap();
-		if (current_cap >= 200) return 0;
+    int AutoSupply::_numSupplyRequired(AeolusBot& aeolusbot)
+    {
+        auto* observation = aeolusbot.Observation();
+        auto& mediator = ManagerMediator::getInstance();
 
-		int num_nexi = mediator.GetOwnTownHalls(aeolusbot).size();
-		int pending_pylons = mediator.GetNumberPending(aeolusbot, ::sc2::UNIT_TYPEID::PROTOSS_PYLON);
-		for (const auto& structure : mediator.GetAllOwnStructures(aeolusbot))
-			if (structure->unit_type == ::sc2::UNIT_TYPEID::PROTOSS_PYLON && structure->build_progress < 1.0f) pending_pylons++;
-		int supply_left = current_cap - observation->GetFoodUsed();
+        int current_cap = observation->GetFoodCap();
+        if (current_cap >= 200) return 0;
 
-		// get all production structures
-		int num_production_structures = 0;
-		for (const auto& structure : mediator.GetAllOwnStructures(aeolusbot))
-		{
-			if (structure->unit_type == ::sc2::UNIT_TYPEID::PROTOSS_GATEWAY
-				|| structure->unit_type == ::sc2::UNIT_TYPEID::PROTOSS_WARPGATE
-				|| structure->unit_type == ::sc2::UNIT_TYPEID::PROTOSS_ROBOTICSFACILITY
-				|| structure->unit_type == ::sc2::UNIT_TYPEID::PROTOSS_STARGATE)
-				num_production_structures++;
-		}
+        int num_nexi = mediator.GetOwnTownHalls(aeolusbot).size();
 
-		if (supply_left <= std::max(2 * num_production_structures, 5))
-		{
-			// std::cout << "Number of pending pylons: " << pending_pylons << " production structures: "
-			//	<< std::ceil(static_cast<float>(num_production_structures) / 2) << std::endl;
-			if (pending_pylons < std::ceil(static_cast<float>(num_production_structures) / 2))
-			{
-				int num = static_cast<int>(std::ceil(static_cast<float>(num_production_structures) / 2) - pending_pylons);
-				return std::min(num, 6);
-			}
-		}
-		else if (num_production_structures == 0 && supply_left <= 2 && pending_pylons == 0)
-		{
-			return 1;
-		}
+        int completed_pylons = 0;
+        int pending_pylons = mediator.GetNumberPending(
+            aeolusbot,
+            ::sc2::UNIT_TYPEID::PROTOSS_PYLON
+        );
 
-		return 0;
-	}
+        for (const auto& structure : mediator.GetAllOwnStructures(aeolusbot))
+        {
+            if (structure->unit_type == ::sc2::UNIT_TYPEID::PROTOSS_PYLON)
+            {
+                if (structure->build_progress < 1.0f)
+                    pending_pylons++;
+                else
+                    completed_pylons++;
+            }
+        }
+
+        // emergency recovery: no pylons and none pending
+        if (completed_pylons == 0 && pending_pylons == 0)
+            return 1;
+
+        int supply_left = current_cap - observation->GetFoodUsed();
+
+        // get all production structures
+        int num_production_structures = 0;
+
+        for (const auto& structure : mediator.GetAllOwnStructures(aeolusbot))
+        {
+            if (structure->unit_type == ::sc2::UNIT_TYPEID::PROTOSS_GATEWAY
+                || structure->unit_type == ::sc2::UNIT_TYPEID::PROTOSS_WARPGATE
+                || structure->unit_type == ::sc2::UNIT_TYPEID::PROTOSS_ROBOTICSFACILITY
+                || structure->unit_type == ::sc2::UNIT_TYPEID::PROTOSS_STARGATE)
+            {
+                num_production_structures++;
+            }
+        }
+
+        if (supply_left <= std::max(2 * num_production_structures, 5))
+        {
+            if (pending_pylons < std::ceil(static_cast<float>(
+                num_production_structures) / 2))
+            {
+                int num = static_cast<int>(
+                    std::ceil(static_cast<float>(
+                        num_production_structures) / 2)
+                    - pending_pylons
+                    );
+
+                return std::min(num, 6);
+            }
+        }
+        else if (num_production_structures == 0
+            && supply_left <= 2
+            && pending_pylons == 0)
+        {
+            return 1;
+        }
+
+        return 0;
+    }
 }
