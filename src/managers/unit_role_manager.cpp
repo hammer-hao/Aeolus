5f78128b74f9ab67178e43b1c5cc088209858ff0
hammer-hao <zihehaomichael@gmail.com>
2026-09-18 11:35:58 -0400

    fix: add tempest and archon to attacking unit types

--- src/managers/unit_role_manager.cpp
+++ src/managers/unit_role_manager.cpp
@@ -92,6 +92,8 @@ namespace Aeolus {
 			|| unit->unit_type == ::sc2::UNIT_TYPEID::PROTOSS_IMMORTAL
 			|| unit->unit_type == ::sc2::UNIT_TYPEID::PROTOSS_COLOSSUS
 			|| unit->unit_type == ::sc2::UNIT_TYPEID::PROTOSS_VOIDRAY
+			|| unit->unit_type == ::sc2::UNIT_TYPEID::PROTOSS_TEMPEST
+			|| unit->unit_type == ::sc2::UNIT_TYPEID::PROTOSS_ARCHON
 			|| unit->unit_type == ::sc2::UNIT_TYPEID::PROTOSS_ZEALOT
 			|| unit->unit_type == ::sc2::UNIT_TYPEID::PROTOSS_ADEPT)
 		{
